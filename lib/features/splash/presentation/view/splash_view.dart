ae6a99ce6011a2d3927298b52bedafcfaf641cae
shalabi11 <alshalabi311@gmail.com>
2025-09-24 09:49:29 +0300

    create splash screen

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:town_pulse/core/utils/styles.dart';
+import 'package:town_pulse/features/splash/presentation/widget/splash_view_body.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.blueGrey[900],
+      body: SplashViewBody(),
+    );
+  }
+}
